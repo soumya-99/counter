45876052f0be9478942222e3a62a51b30762a640
Soumyadeep Mondal <soumyadeepmondal1234@gmail.com>
2021-01-04 11:58:24 +0530

    [MR.3.7] Rendering Classes Dynamically

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -7,15 +7,16 @@ export default class Counter extends Component {
 	render() {
 		return (
 			<div>
-				<span
-					style={{ fontSize: 50, fontWeight: "bold" }}
-					className="badge badge-primary m-2"
-				>
-					{this.formatCount()}
-				</span>
+				<span className={this.getBadgeClasses()}>{this.formatCount()}</span>
 				<button className="btn btn-secondary btn-sm">Increment</button>
 			</div>
 		);
+	}
+
+	getBadgeClasses() {
+		let classes = "badge m-2 badge-";
+		classes += this.state.count === 0 ? "warning" : "primary";
+		return classes;
 	}
 
 	formatCount() {

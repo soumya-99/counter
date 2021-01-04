db20c7121f4940888b2e567e928c929f45200d5d
Soumyadeep Mondal <soumyadeepmondal1234@gmail.com>
2021-01-04 14:27:18 +0530

    [MR.4.3] Passing Data Through Components

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,14 +2,15 @@ import React, { Component } from "react";
 
 export default class Counter extends Component {
 	state = {
-		count: 0,
+		value: this.props.value,
 	};
 
 	handleIncrement = () => {
-		this.setState({ count: this.state.count + 1 });
+		this.setState({ value: this.state.value + 1 });
 	};
 
 	render() {
+
 		return (
 			<div>
 				<span className={this.getBadgeClasses()}>{this.formatCount()}</span>
@@ -25,12 +26,12 @@ export default class Counter extends Component {
 
 	getBadgeClasses() {
 		let classes = "badge m-2 badge-";
-		classes += this.state.count === 0 ? "warning" : "primary";
+		classes += this.state.value === 0 ? "warning" : "primary";
 		return classes;
 	}
 
 	formatCount() {
-		const { count } = this.state;
-		return count === 0 ? "Zero" : count;
+		const { value } = this.state;
+		return value === 0 ? "Zero" : value;
 	}
 }

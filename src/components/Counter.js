af10aa10fa2101504019109fa4f9ca4e459e2573
Soumyadeep Mondal <soumyadeepmondal1234@gmail.com>
2021-01-04 11:49:36 +0530

    [MR.3.6] Setting Attributes

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -7,8 +7,13 @@ export default class Counter extends Component {
 	render() {
 		return (
 			<div>
-				<span>{this.formatCount()}</span>
-				<button>Increment</button>
+				<span
+					style={{ fontSize: 50, fontWeight: "bold" }}
+					className="badge badge-primary m-2"
+				>
+					{this.formatCount()}
+				</span>
+				<button className="btn btn-secondary btn-sm">Increment</button>
 			</div>
 		);
 	}

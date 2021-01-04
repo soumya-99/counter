eb903dd7fbf5a24acd6ddeae3dd48de3791dff41
Soumyadeep Mondal <soumyadeepmondal1234@gmail.com>
2021-01-04 12:12:32 +0530

    [MR.3.9] Conditional Rendering

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,30 +3,39 @@ import React, { Component } from "react";
 export default class Counter extends Component {
 	state = {
 		count: 0,
-		tags: ["tag1", "tag2", "tag3"],
+		tags: [],
 	};
+
+	renderTags() {
+		if (this.state.tags.length === 0) return <p>There are no tags</p>;
+		return (
+			<ul>
+				{this.state.tags.map((tag) => (
+					<li key={tag}>{tag}</li>
+				))}
+			</ul>
+		);
+	}
+
 	render() {
 		return (
 			<div>
-				<span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-				<button className="btn btn-secondary btn-sm">Increment</button>
-				<ul>
-					{this.state.tags.map((tag) => (
-						<li key={tag}>{tag}</li>
-					))}
-				</ul>
+				{/* <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+				<button className="btn btn-secondary btn-sm">Increment</button> */}
+                {this.state.tags.length === 0 && "Please Create a New Tag"}
+				{this.renderTags()}
 			</div>
 		);
 	}
 
-	getBadgeClasses() {
-		let classes = "badge m-2 badge-";
-		classes += this.state.count === 0 ? "warning" : "primary";
-		return classes;
-	}
+	// getBadgeClasses() {
+	// 	let classes = "badge m-2 badge-";
+	// 	classes += this.state.count === 0 ? "warning" : "primary";
+	// 	return classes;
+	// }
 
-	formatCount() {
-		const { count } = this.state;
-		return count === 0 ? "Zero" : count;
-	}
+	// formatCount() {
+	// 	const { count } = this.state;
+	// 	return count === 0 ? "Zero" : count;
+	// }
 }

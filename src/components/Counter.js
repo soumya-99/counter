fdd0cfaf17425ee1956a950bd53eaf8e87603fe7
Soumyadeep Mondal <soumyadeepmondal1234@gmail.com>
2021-01-04 12:17:04 +0530

    [MR.3.10] Handelling Events

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,39 +3,29 @@ import React, { Component } from "react";
 export default class Counter extends Component {
 	state = {
 		count: 0,
-		tags: [],
-	};
-
-	renderTags() {
-		if (this.state.tags.length === 0) return <p>There are no tags</p>;
-		return (
-			<ul>
-				{this.state.tags.map((tag) => (
-					<li key={tag}>{tag}</li>
-				))}
-			</ul>
-		);
-	}
+    };
+    
+    handleIncrement() {
+        console.log("Increment CLicked", this);
+    }
 
 	render() {
 		return (
 			<div>
-				{/* <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-				<button className="btn btn-secondary btn-sm">Increment</button> */}
-                {this.state.tags.length === 0 && "Please Create a New Tag"}
-				{this.renderTags()}
+				<span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+				<button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
 			</div>
 		);
 	}
 
-	// getBadgeClasses() {
-	// 	let classes = "badge m-2 badge-";
-	// 	classes += this.state.count === 0 ? "warning" : "primary";
-	// 	return classes;
-	// }
+	getBadgeClasses() {
+		let classes = "badge m-2 badge-";
+		classes += this.state.count === 0 ? "warning" : "primary";
+		return classes;
+	}
 
-	// formatCount() {
-	// 	const { count } = this.state;
-	// 	return count === 0 ? "Zero" : count;
-	// }
+	formatCount() {
+		const { count } = this.state;
+		return count === 0 ? "Zero" : count;
+	}
 }
